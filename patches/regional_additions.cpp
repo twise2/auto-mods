af3c3975dc07664bc25fc4ab6f0b6ca197a7aee0
Tristan Wise <tristan@tristanwise.com>
2023-12-12 20:30:03 -0500

    Add a number of further regional upgrades, longboats ability to garrison, and samurais ability to swap to a ranged mode that functions only as an anti-UU mode

--- patches/regional_additions.cpp
+++ patches/regional_additions.cpp
@@ -5,37 +5,233 @@
 
 //The idea to this mod is not to affect anything gameplay/balance wise 
 //but only to give civs access to technology / units they would have had
-//access to historically, take away things they would not have, and provide
-//more of a regional identity to more of the civs. The premise behind the mod
-//is to do things like the following
-//
-//Give new features to civs that should have them. For example, nomadic civs having
-//trade cart access.
-//
-//Differentiate steppe and non-steppe civs to give a regional feel to those units
-//
-//Grant gunpowder to Chinese as they were on of the first to utilize it
-//
-//Grant Caravanseri to civs that had it.
-//
-//etc....
+//access to historically, and provide more of a regional identity to more of the civs. 
+//The premise behind the mod is the following
+
+// 1. Give civs access to new elements they likely would have had if released today 
+// 2. Stay historically accurate to the extent possible 
+// 3. Focus on giving factions/regions more of an identity/uniqueness
+// 4. Try to be additive (devs give trebs to civs without them for a reason)
+// 5. If balance occurs, generally err on buffing instead of nerfing
+
+void duplicateTechToNewCiv(genie::DatFile *df, int techId, int civId ) {
+        genie::Tech tech = df->Techs.at(techId);
+        tech.Civ = civId;
+        //turn off full tech mode as that will break with this anyway
+        //and this will be so much easier if i can reuse FTT stuff in base
+        //rather than having to figure out tech tree modifications
+        tech.FullTechMode = 0;         
+        df->Techs.push_back(tech);
+    std::cout << "Added Tech '" << tech.Name << "' to civ id '" << df->Civs.at(civId).Name << "'" << std::endl;
+};
+
+void allowSamuraiToSwapToArcherMode(genie::DatFile *df) {
+    for (genie::Civ &civ : df->Civs) {
+        //use archer fo the eye for samurai swap
+        genie::Unit &samurai = civ.Units.at(SAMURAI);
+        samurai.Nothing = ARCHER_OF_THE_EYES;
+        samurai.Trait = samurai.Trait | 8;
+
+        //make archer of the eye bad except against unique units
+        genie::Unit &samuraiArcher = civ.Units.at(ARCHER_OF_THE_EYES);
+        samuraiArcher.Name = "Samurai (ranged)";
+        samuraiArcher.Creatable.HeroGlowGraphic = -1;
+        samuraiArcher.Creatable.HeroMode = 0;
+        samuraiArcher.Type50.BaseArmor = 0; //dont give armor bonuses
+        //make sure it's ranged mode is only good against unique units
+        samuraiArcher.Type50.DisplayedAttack = 1;
+        //super slow but heavy hitting to encourage shoot once than swap
+        samuraiArcher.Type50.ReloadTime = 4;
+        samuraiArcher.Type50.DisplayedReloadTime = 4;
+        //has 2 attacks, unlikely to change
+        samuraiArcher.Type50.Attacks.at(0).Amount = 1;
+        samuraiArcher.Type50.Attacks.at(1).Amount = 25;
+        samuraiArcher.Type50.Attacks.at(1).Class = ATTACK_TYPE_HERO_BONUS_DAMAGE;
+        //slow them down for good measure to decrease odds of staying in this form
+        samuraiArcher.Speed = .81;
+    }
+    std::cout << "Added Ability 'Samurai swap to Ranged Mode'" << std::endl;
+}
+
+void addTechnologiesToCivs(genie::DatFile *df, std::vector<int16_t> civIds, std::vector<int16_t>  techIds){
+    for (int16_t civId : civIds) {
+        for (int16_t techId : techIds) {
+            duplicateTechToNewCiv(df, techId, civId);
+        }
+    };
+};
+
+void makeLongboatsTransports(genie::DatFile *df) {
+    //allow viking longboats to transport 5 units
+    //https://www.google.com/search?q=longboats+should+transport+aoe2&rlz=1C5CHFA_enUS906US906&oq=longboats+should+transport+aoe2&gs_lcrp=EgZjaHJvbWUyCQgAEEUYORigATIHCAEQIRigAdIBCDQyOTBqMGo5qAIAsAIA&sourceid=chrome&ie=UTF-8
+    for (genie::Civ &civ : df->Civs) {
+        genie::Unit &longboat = civ.Units.at(LONGBOAT);
+        genie::Unit &eliteLongboat = civ.Units.at(ELITE_LONGBOAT);
+        longboat.GarrisonCapacity = 5;
+        eliteLongboat.GarrisonCapacity = 5;
+        auto unloadTask = new genie::Task();
+        unloadTask->ActionType = ACTION_TYPE_UNLOAD;
+        longboat.Bird.TaskList.push_back(*unloadTask);
+        eliteLongboat.Bird.TaskList.push_back(*unloadTask);
+    }
+    std::cout << "Added Ability 'Longboats act as Transports'" << std::endl;
+}
+
+void giveHistoricRegionalVarietyToCivs(genie::DatFile *df) {
+    //Add Mulecarts to the nomadic civs for that flavor
+    //https://www.reddit.com/r/aoe2/comments/17stxfy/should_all_nomad_civs_be_given_mule_carts/
+    addTechnologiesToCivs(df, {
+                CIV_MONGOLS,
+                CIV_HUNS,
+                CIV_TATARS,
+                CIV_CUMANS,
+                CIV_MAGYARS
+            }, {
+                TECH_MULECART_MAKE_AVAILABLE
+            } 
+    );
+    //add chemistry to the civ that created it 
+    //https://forums.ageofempires.com/t/chinese-civilization/73726
+    addTechnologiesToCivs(df, {
+                CIV_CHINESE,
+            }, {
+               TECH_HAND_CANNON,
+               TECH_BOMBARD_TOWER
+            } 
+    );
+    //add caravanseri to silk road civs 
+    //https://www.reddit.com/r/aoe2/comments/10a3jg9/historically_persian_should_also_have_access_to/
+    addTechnologiesToCivs(df, {
+                CIV_SARACENS,
+                CIV_CHINESE,
+                CIV_MONGOLS,
+                CIV_TURKS,
+                CIV_TATARS
+            }, {
+               CHEMISTRY 
+            } 
+    );
+    //add elephaunt archers to civs that had them historically
+    //https://www.reddit.com/r/aoe2/comments/10mqm64/sotl_should_more_civs_get_elephant_archers/
+    addTechnologiesToCivs(df, {
+                CIV_PERSIANS,
+                CIV_BURMESE,
+                CIV_MALAY,
+                CIV_KHMER,
+                CIV_VIETNAMESE
+            }, {
+                TECH_ELEPHAUNT_ARCHER_MAKE_AVAILABLE
+            } 
+    );
+    //give armoured elephaunts to elephant civs 
+    //https://www.reddit.com/r/aoe2/comments/ubkjoa/armored_elephants_for_khmer_burmese_and_malay
+    addTechnologiesToCivs(df, {
+                CIV_KHMER,
+                CIV_BURMESE,
+                CIV_BURMESE,
+                CIV_MALAY,
+                CIV_ETHIOPIANS
+            }, {
+            TECH_ARMORED_ELEPHANT,
+            TECH_SIEGE_ELEPHANT
+    });
+    //add steppe lancers to civs that should probably have them
+    //https://www.reddit.com/r/aoe2/comments/10mqm64/sotl_should_more_civs_get_elephant_archers/
+    //https://www.reddit.com/r/aoe2/comments/y6b17r/disproportionate_spread_of_regional_units/
+    addTechnologiesToCivs(df, {
+                CIV_CHINESE,
+                CIV_BULGARIANS,
+                CIV_LITHUANIANS, 
+                CIV_HINDUSTANIS, 
+                CIV_MAGYARS,
+                CIV_SLAVS,
+                CIV_HUNS,
+                CIV_TURKS,
+                CIV_PERSIANS
+            }, {
+                TECH_STEPPE_LANCER_MAKE_AVAILABLE
+    });
+    addTechnologiesToCivs(df, {
+                CIV_HUNS,
+                CIV_TURKS
+            }, {
+                TECH_ELITE_STEPPE_LANCER_MAKE_AVAILABLE
+    });
+    //disable paladin for pure cumans and give heavy camel
+    //https://forums.ageofempires.com/t/give-cumans-heavy-camel-riders-and-remove-paladins-and-maybe-chevaliers/196455
+    //https://www.reddit.com/r/aoe2/comments/y6b17r/disproportionate_spread_of_regional_units/
+    addTechnologiesToCivs(df, {
+                CIV_CUMANS,
+                CIV_HUNS
+            }, {
+                TECH_DISABLE_PALADIN,
+                TECH_HEAVY_CAMEL_RIDER
+    });
+    //Add genitour to more civs that would have had them
+    //https://forums.ageofempires.com/t/give-cumans-heavy-camel-riders-and-remove-paladins-and-maybe-chevaliers/196455
+    addTechnologiesToCivs(df, {
+                CIV_SPANISH,
+                CIV_PORTUGUESE,
+                CIV_PERSIANS,
+                CIV_SARACENS,
+                CIV_TURKS
+            }, {
+                TECH_GENITOUR_MAKE_AVAILABLE
+    });
+
+    //Give more civs access to the camel scout
+    //https://www.reddit.com/r/aoe2/comments/ukg4ko/should_other_civs_get_the_camel_scout_as_well
+    addTechnologiesToCivs(df, {
+                CIV_HINDUSTANIS,
+                CIV_BERBERS,
+                CIV_SARACENS,
+            }, {
+                TECH_CAMEL_SCOUT_MAKE_AVAILABLE
+    });
+    //Give out some legionaires 
+    //https://www.reddit.com/r/aoe2/comments/13tw143/so_i_read_some_comments_about_how_byzantine_civ/
+    addTechnologiesToCivs(df, {
+                CIV_BYZANTINES,
+            }, {
+                TECH_LEGIONARY,
+                TECH_DISABLE_MILITIA_UPGRADES
+    });
+    //give celts some infantry love and remove paladin
+    //https://www.reddit.com/r/aoe2/comments/164jd1v/give_celts_squires/
+    addTechnologiesToCivs(df, {
+                CIV_CELTS,
+            }, {
+                SQUIRES,
+                BLOODLINES,
+                TECH_DISABLE_PALADIN
+    });
+    //give magyars winged hussars
+    //https://www.reddit.com/r/aoe2/comments/qu6b4a/should_magyars_get_winged_hussars_too/
+    addTechnologiesToCivs(df, {
+                CIV_MAGYARS,
+                CIV_CUMANS
+            }, {
+            TECH_WINGED_HUSSARS,
+            TECH_DISABLE_HUSSARS
+    });
+    //give ethiopians war elephants, no elite
+    //https://forums.ageofempires.com/t/should-ethiopians-get-war-elephants/202217/12
+    addTechnologiesToCivs(df, {
+                CIV_ETHIOPIANS
+            }, {
+            TECH_WAR_ELEPHANT_MAKE_AVAILABLE
+    });
+    //give imperial skirm to civs that had great skirms 
+    //https://www.reddit.com/r/aoe2/comments/17h70lv/imperial_skirmisher_would_be_nice_if_it_wasnt/
+    addTechnologiesToCivs(df, {
+                CIV_MALIANS,
+                CIV_ROMANS
+            }, {
+            TECH_IMPERIAL_SKIRMISHER
+    });
+}
 
 void configureRegionalAdditions(genie::DatFile *df) {
-    std::set<int> villagers = {
-        ID_FISHING_SHIP,        ID_TRADE_COG,           ID_TRADE_CART_EMPTY,   ID_TRADE_CART_FULL,
-        ID_VILLAGER_BASE_M,     ID_VILLAGER_BASE_F,     ID_VILLAGER_FARMER_M,  ID_VILLAGER_FARMER_F,
-        ID_VILLAGER_SHEPHERD_M, ID_VILLAGER_SHEPHERD_F, ID_VILLAGER_FORAGER_M, ID_VILLAGER_FORAGER_F,
-        ID_VILLAGER_HUNTER_M,   ID_VILLAGER_HUNTER_F,   ID_VILLAGER_FISHER_M,  ID_VILLAGER_FISHER_F,
-        ID_VILLAGER_WOOD_M,     ID_VILLAGER_WOOD_F,     ID_VILLAGER_GOLD_M,    ID_VILLAGER_GOLD_F,
-        ID_VILLAGER_STONE_M,    ID_VILLAGER_STONE_F,    ID_VILLAGER_BUILDER_M, ID_VILLAGER_BUILDER_F,
-        ID_VILLAGER_REPAIRER_M, ID_VILLAGER_REPAIRER_F,
-    };
-
-    for (genie::Civ &civ : df->Civs) {
-        for (int villager_id : villagers) {
-            civ.Units.at(villager_id).DeadUnitID = ID_SABOTEUR;
-            std::cout << "Patched Villager unit " << villager_id << " for civ " << civ.Name << "\n";
-        }
-        std::cout << "Patched Saboteur unit for civ " << civ.Name << "\n";
-    }
+    makeLongboatsTransports(df);
+    giveHistoricRegionalVarietyToCivs(df);
 }
