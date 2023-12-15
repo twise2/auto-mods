40689058843b08b40b237c2f3eaa9048696f318e
Tristan Wise <tristan@tristanwise.com>
2023-12-14 19:36:11 -0500

    add script to just build all my stuff at once into downloads folder

---
+++ scripts/regional_exploding_snipes.sh
@@ -0,0 +1,3 @@
+./build/create-data-mod regional-additions ~/Downloads/empires2_x2_p1.dat  ~/Desktop/mod_test.dat
+./build/create-data-mod exploding-kings ~/Desktop/mod_test.dat ~/Desktop/mod_test_ek.dat
+./build/create-data-mod rewarding_snipes ~/Desktop/mod_test_ek.dat ~/Desktop/mod_test_ek_rw.dat
